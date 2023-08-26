25fc424287c63b217092f1bf19dc231681fb40ac
Solacexcv <143187408+Solacexcv@users.noreply.github.com>
2023-08-26 21:01:38 +0800

    Update TMHelper.sh

--- TMHelper.sh
+++ TMHelper.sh
@@ -157,9 +157,8 @@ reset="\033[0m"
         7)
             type_animation "\nYou selected: Trace Exact Location"
             type_animation "Please Wait Trace Exact Location is Installing... "
-            git clone https://github.com/thewhiteh4t/seeker || cd seeker
-            cd seeker && wget https://github.com/thewhiteh4t/seeker/blob/master/install.sh && chmod +x install.sh && ./install.sh
-            python3 seeker.py
+            green_text_animation "Trace Exact Location done installing to run this tool exit TMHelper || cd seeker || chmod +x install.sh || ./install.sh or bash install.sh || python3 seeker.py
+            git clone https://github.com/thewhiteh4t/seeker
             ;;
         8)
             type_animation "\nYou selected: Cam-Dumper"
